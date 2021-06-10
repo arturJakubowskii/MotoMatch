b64f934d84a349696112f0a02c1413b28590c1f1
Artur Jakubowski <a.jakubowskipl@gmail.com>
2021-06-10 13:02:04 +0200

    Initial commit

---
+++ src/main/java/com/motomatch/motomatch/MotomatchApplication.java
@@ -0,0 +1,13 @@
+package com.motomatch.motomatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotomatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotomatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motomatch/motomatch/MotomatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motomatch.motomatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotomatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
