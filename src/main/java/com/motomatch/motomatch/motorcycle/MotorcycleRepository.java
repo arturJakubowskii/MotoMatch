dfa068fe294bd2ba5a7e0b03452b2e9421bfdf3b
Artur Jakubowski <a.jakubowskipl@gmail.com>
2021-06-12 21:04:46 +0200

    Create Motorcycle class with constructor parameters

--- src/main/java/com/motomatch/motomatch/motorcycle/MotorcycleRepository.java
+++ src/main/java/com/motomatch/motomatch/motorcycle/MotorcycleRepository.java
@@ -1,4 +1,8 @@
 package com.motomatch.motomatch.motorcycle;
 
-public interface MotorcycleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotorcycleRepository
+        extends JpaRepository<Motorcycle,Long> {
+
 }
