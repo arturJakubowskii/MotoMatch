df6c2fe2b2286d7417894d439eeef813e1384d3b
Artur Jakubowski <a.jakubowskipl@gmail.com>
2021-06-17 18:27:08 +0200

    Delete comment

--- src/main/java/com/motomatch/motomatch/motorcycle/MotorcycleRepository.java
+++ src/main/java/com/motomatch/motomatch/motorcycle/MotorcycleRepository.java
@@ -8,9 +8,6 @@ import java.util.List;
 public interface MotorcycleRepository
         extends JpaRepository<Motorcycle,Long> {
 
-//    @Query("SELECT s FROM Student s WHERE s.name = ?1")
-//    Optional<Student> findStudentByName(String email);
-
     @Query("SELECT m FROM Motorcycle m WHERE m.drivingLicenseType = ?1")
     List<Motorcycle> findMotorcycleByDrivingLicenseType(String type);
 
